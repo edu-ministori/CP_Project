0b10ca30e1386e6b5230d54ec5331133e965fa7d
edu-ministori <edu.ministori@gmail.com>
2023-09-20 12:14:58 +0900

    source

--- CP_Project/CP_Project/CP_Project.cpp
+++ CP_Project/CP_Project/CP_Project.cpp
@@ -15,12 +15,15 @@
 #include "Auto.h"
 #include "Ch14_VirtualChild.h"
 #include "Ch14_VirtualParent.h"
+#include "Ch15_PureVirtualParent.h"
+#include "Ch15_PureVirtualChild.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
 
     // class 정적 선언
+    /*
     Vehicle Ray(2023, 12000000);
     Ray.PrintPrice();
 
@@ -32,25 +35,28 @@ int main()
     cout << Ray.GetYear() << endl;
 
     Vehicle Sonata;
-
+    */
 
     // class 동적 선언(생성)
+    /*
     Animal* Dog = new Animal;
     Dog->PrintFinger();
 
     delete Dog;
-
+    */
     // 자식 클래스 생성
     Atv Moto;
 
     // 상속받은 Bike class의 멤버
+    /*
     Moto.PrintTire();
     cout << Moto.Price << endl;
-
+    */
     // Atv class 멤버
     Moto.PrintPrice();
 
     // 함수 오버로딩
+    /*
     Arithmatic Expression;
 
     Expression.Sum(50);
@@ -58,18 +64,18 @@ int main()
     Expression.Sum(50.123f);
 
     Expression.Sum(60, 70);
-
+    */
     // 함수 오버라이딩
     Moto.Print();
 
     // 생성자가 명시적으로 선언
     // 자식 클래스 선언 : 명시적 선언
-
+    /*
     cout << "-----------------------------" << endl;
     Auto Mobile;
 
     Auto Avante(2024, 10000000);
-
+    */
 
     cout << "-----------------------------" << endl;
     //ArrayClass();
@@ -101,6 +107,15 @@ int main()
     Parent2 = Child2;
 
     Parent2->PrintClass();
+
+    cout << "-----------------------------" << endl;
+
+    // PureVirtualParent PureParent; => 추상클래스는 단독으로 객체 생성할 수 없음
+    // 추상 클래스를 상속받는 자식 클래스는 순수 가상함수를 재정의 해야함
+    PureVirtualChild PureChild;
+
+    PureChild.Print();
+    PureChild.PrintClass();
 
 
 }
